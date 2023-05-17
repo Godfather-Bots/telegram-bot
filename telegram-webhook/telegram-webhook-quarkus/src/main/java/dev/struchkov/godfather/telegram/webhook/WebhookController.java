f7c6a9099bb32b189af93dfe9c071a52d7d6594f
Struchkov Mark <mark@struchkov.dev>
2023-05-17 18:49:24 +0300

    Добавил моментальный ответ в WebhookController

--- telegram-webhook/telegram-webhook-quarkus/src/main/java/dev/struchkov/godfather/telegram/webhook/WebhookController.java
+++ telegram-webhook/telegram-webhook-quarkus/src/main/java/dev/struchkov/godfather/telegram/webhook/WebhookController.java
@@ -3,18 +3,17 @@ package dev.struchkov.godfather.telegram.webhook;
 import dev.struchkov.godfather.telegram.domain.config.TelegramBotConfig;
 import dev.struchkov.godfather.telegram.quarkus.context.service.EventDistributor;
 import io.smallrye.mutiny.Uni;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.HeaderParam;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import lombok.extern.slf4j.Slf4j;
 import org.telegram.telegrambots.meta.api.objects.Update;
-
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.HeaderParam;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
 import static dev.struchkov.haiti.context.exception.AccessException.accessException;
 import static dev.struchkov.haiti.utils.Inspector.isTrue;
@@ -38,17 +37,20 @@ public class WebhookController {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Uni<Response> updateReceived(@PathParam("webhookPath") String botPath, @HeaderParam("X-Telegram-Bot-Api-Secret-Token") String secretTokenFromTelegram, Update update) {
-        return Uni.createFrom().voidItem()
+        Uni.createFrom().voidItem()
                 .invoke(() -> {
                     log.trace("Получено webhook событие: {}", update);
                     isTrue(PATH_KEY.equals(botPath), accessException(ERROR_ACCESS));
                     isTrue(secretToken.equals(secretTokenFromTelegram), accessException(ERROR_ACCESS));
                 })
                 .call(() -> eventDistributor.processing(update))
-                .onFailure().invoke(th -> log.error("При обработке webhook события произошла ошибка. Идентификатор события: " + update.getUpdateId(), th))
-                .onFailure().recoverWithNull()
-                .invoke(() -> log.trace("Сообщили Telegram, что вебхук событие обработано. Идентификатор события: {}", update.getUpdateId()))
-                .map(ignored -> Response.ok().build());
+                .subscribe().with(
+                        v -> log.trace("Webhook событие обработано. Идентификатор: {}", update.getUpdateId()),
+                        fail -> log.error("При обработке webhook события произошла ошибка. Идентификатор события: " + update.getUpdateId(), fail)
+                );
+
+        return Uni.createFrom().item(Response.ok().build())
+                .invoke(() -> log.trace("Сообщили Telegram, что вебхук событие принято. Идентификатор события: {}", update.getUpdateId()));
     }
 
     @GET
